653255134c85d6320d91e9dbdf5c577daf6b2815
Ajay Patel <61546968+ajp4891@users.noreply.github.com>
2024-10-07 03:42:42 -0700

    Time: 42 ms (85.98%) | Memory: 17 MB (54.40%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,21 +1,37 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        # if len(s) != len(t):
+        #     return False
+
+        # d = dict()
+
+        # for l in s:
+        #     if l not in d:
+        #         d[l] = 0
+        #     d[l] += 1
+
+        # for l in t:
+        #     if l not in d or d[l] <= 0:
+        #         return False
+        #     else:
+        #         d[l] -= 1
+        #         if d[l] == 0:
+        #             del d[l]
+
+        # return not d
+
         if len(s) != len(t):
             return False
 
-        d = dict()
+        sets = set(s)
+        
+        if sets != set(t):
+            return False
 
-        for l in s:
-            if l not in d:
-                d[l] = 0
-            d[l] += 1
 
-        for l in t:
-            if l not in d or d[l] <= 0:
+
+        for x in sets:
+            if s.count(x) != t.count(x):
                 return False
-            else:
-                d[l] -= 1
-                if d[l] == 0:
-                    del d[l]
 
-        return not d
+        return True
