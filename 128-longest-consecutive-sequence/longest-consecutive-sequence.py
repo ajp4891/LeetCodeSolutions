83231c8be6d1d03af52a285994d944808ed6c2e8
Ajay Patel <61546968+ajp4891@users.noreply.github.com>
2024-10-07 14:03:32 -0700

    Time: 364 ms (53.83%) | Memory: 35.7 MB (9.59%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -1,33 +1,40 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         track = set(nums)
-        visited = set()
-        mxseq = 0
+        if len(track) <= 1:
+            return len(track)
+        
+        ans = set(nums)
+
+
         for n in track:
-            if n not in visited:
-                visited.add(n)
-                front = back = n
-                flagf = flagb = False
-                seq = 1
-                while True:
-                    if not flagf:
-                        front += 1
-                        if front not in visited and front in track:
-                            seq += 1
-                            visited.add(front)
-                        else:
-                            flagf = True
+            if n-1 not in track and n+1 not in track:
+                ans.remove(n)
 
-                    if not flagb:
-                        back -= 1
-                        if back not in visited and back in track:
-                            seq += 1
-                            visited.add(back)
-                        else:
-                            flagb = True
+        mxseq = 1
+        while ans:
+            front = back = ans.pop()
+            flagf = flagb = False
+            seq = 1
+            while True:
+                if not flagf:
+                    front += 1
+                    if front in ans:
+                        ans.remove(front)
+                        seq += 1
+                    else:
+                        flagf = True
 
-                    if flagb and flagf:
-                        break
+                if not flagb:
+                    back -= 1
+                    if back in ans:
+                        ans.remove(back)
+                        seq += 1
+                    else:
+                        flagb = True
+
+                if flagb and flagf:
+                    break
                 
             if seq > mxseq:
                 mxseq = seq
