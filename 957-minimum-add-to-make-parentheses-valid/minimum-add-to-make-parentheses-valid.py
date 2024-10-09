5b7599419498cb0ad504f81fae8595d33e6388a1
Ajay Patel <61546968+ajp4891@users.noreply.github.com>
2024-10-08 21:02:54 -0700

    Time: 40 ms (20.46%) | Memory: 16.5 MB (71.14%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
@@ -1,15 +1,29 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
-        openb = 0
-        req = 0
+        # openb = 0
+        # req = 0
 
-        for c in s:
-            if c == "(":
-                openb += 1
-            else:
-                if openb > 0:
-                    openb -= 1
-                else:
-                    req += 1
+        # for c in s:
+        #     if c == "(":
+        #         openb += 1
+        #     else:
+        #         if openb > 0:
+        #             openb -= 1
+        #         else:
+        #             req += 1
 
-        return openb + req
+        # return openb + req
+        need = 0 # number neede for ')'
+        res = 0
+
+        for p in s:
+            if p == '(':
+                need += 1
+            
+            if p == ')':
+                need -= 1
+
+                if need == -1:
+                    res += 1
+                    need = 0
+        return res + need
