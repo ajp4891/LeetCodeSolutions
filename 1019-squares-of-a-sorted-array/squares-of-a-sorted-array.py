efd9da7eac502fc362b2876fb1f6808699cc7c31
Ajay Patel <61546968+ajp4891@users.noreply.github.com>
2024-10-08 22:54:33 -0700

    Time: 148 ms (86.53%) | Memory: 18.9 MB (32.97%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -1,14 +1,6 @@
 import math
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        # final = []
-        # for n in nums:
-        #     sq = n * n
-        #     i = bisect.bisect_left(final, sq)
-        #     final.insert(i, sq)
-
-        # return final
-
         i = 0
         n = len(nums)
         j = n - 1
