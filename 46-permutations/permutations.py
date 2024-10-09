a67757cab9ecf0e26094395a6c96bb5abbdb99f5
Ajay Patel <61546968+ajp4891@users.noreply.github.com>
2024-10-09 02:42:40 -0700

    Time: 34 ms (90.64%) | Memory: 16.7 MB (38.87%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,21 +1,35 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        res = []
+        # res = []
 
-        def backtrack(left: int, curr: List[int]):
-            if left == 0 and len(curr) == len(nums):
-                res.append(list(curr))
-                return
+        # def backtrack(left: int, curr: List[int]):
+        #     if left == 0 and len(curr) == len(nums):
+        #         res.append(list(curr))
+        #         return
 
-            for i in range(len(nums)):
-                cval = nums[i]
-                if cval == None:
-                    continue
-                curr.append(cval)
-                nums[i] = None
-                backtrack(left - 1, curr)
-                nums[i] = cval
-                curr.pop()
+        #     for i in range(len(nums)):
+        #         cval = nums[i]
+        #         if cval == None:
+        #             continue
+        #         curr.append(cval)
+        #         nums[i] = None
+        #         backtrack(left - 1, curr)
+        #         nums[i] = cval
+        #         curr.pop()
 
-        backtrack(len(nums), [])
-        return res
+        # backtrack(len(nums), [])
+        # return res
+        result = []
+        if len(nums) == 1:
+            return [nums[:]]
+        
+        for i in range(len(nums)):
+            n = nums.pop(0)
+            
+            perms = self.permute(nums)
+
+            for perm in perms:
+                perm.append(n)
+            result.extend(perms)
+            nums.append(n)
+        return result
