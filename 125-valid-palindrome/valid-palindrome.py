3f5a3dd648da39e3775222cd7a65b45999d6dd59
Ajay Patel <61546968+ajp4891@users.noreply.github.com>
2024-10-08 23:26:35 -0700

    Time: 40 ms (81.74%) | Memory: 17 MB (86.25%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,14 +1,20 @@
 import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = re.sub("[\W_]", "", s).lower()
+        # s = re.sub("[\W_]", "", s).lower()
+        s = s.lower()
         i, j = 0, len(s) - 1
 
         while i < j:
+            if not s[i].isalnum():
+                i += 1
+                continue
+            if not s[j].isalnum():
+                j -= 1
+                continue
             if s[i] != s[j]:
                 return False
             i += 1
             j -= 1
 
         return True
-        
