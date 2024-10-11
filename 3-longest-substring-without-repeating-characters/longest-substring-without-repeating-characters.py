2810a721d4c38a6bd1ca7c7fa1b11a5221d83447
Ajay Patel <61546968+ajp4891@users.noreply.github.com>
2024-10-11 05:09:20 -0700

    Time: 60 ms (39.02%) | Memory: 16.8 MB (12.67%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -14,20 +14,3 @@ class Solution:
             r += 1
 
         return ln
-
-
-
-        # if not s:
-        #     return 0
-        # seen = {}
-
-        # l, r = 0, 0
-        # ln = 1
-        # while r < len(s):
-        #     if s[r] in seen:
-        #         l = max(l, seen[s[r]] + 1)
-        #     ln = max(ln, r - l + 1)
-        #     seen[s[r]] = r
-        #     r += 1
-
-        # return ln
