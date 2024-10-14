1476be835062e24c668807a15af6ab1d469e4abc
Ajay Patel <61546968+ajp4891@users.noreply.github.com>
2024-10-14 03:24:00 -0700

    Time: 1126 ms (24.62%) | Memory: 43.1 MB (33.17%) - LeetSync

---
+++ 2588-maximum-number-of-points-from-grid-queries/maximum-number-of-points-from-grid-queries.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maxPoints(self, grid: List[List[int]], queries: List[int]) -> List[int]:
+        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)] # b, r, t, l
+        k = len(queries)
+
+        ans = [0] * k
+
+        queries_sorted = [[queries[i], i] for i in range(k)]
+        queries_sorted.sort()
+        
+
+        visited = {(0, 0)}
+        q = [[grid[0][0], 0, 0]]
+        score = 0
+
+        for a, ind in queries_sorted:
+            while q and q[0][0] < a:
+                score += 1
+                _, i, j = heapq.heappop(q)
+                
+                for di, dj in directions:
+                    i2 = di + i
+                    j2 = dj + j
+                    if 0 <= i2 < len(grid) and 0 <= j2 < len(grid[0]):
+                        if (i2, j2) not in visited:
+                            heapq.heappush(q, [grid[i2][j2], i2, j2])
+                            visited.add((i2, j2))
+
+            ans[ind] = score
+
+        return ans
