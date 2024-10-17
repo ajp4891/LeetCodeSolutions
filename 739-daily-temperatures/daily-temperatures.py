c3647f5bb4998a58a27b73e8c546054d74094558
Ajay Patel <61546968+ajp4891@users.noreply.github.com>
2024-10-17 03:04:58 -0700

    Time: 898 ms (53.21%) | Memory: 32.3 MB (15.71%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,15 +1,26 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         n = len(temperatures)
-        ans = [0] * n
-        heap = [(temperatures[0], 0)]
+        # ans = [0] * n
+        # heap = [(temperatures[0], 0)]
 
-        for i in range(1, n):
-            t = temperatures[i]
-            while heap and t > heap[0][0]:
-                _, ind = heapq.heappop(heap)
-                ans[ind] = i - ind
+        # for i in range(1, n):
+        #     t = temperatures[i]
+        #     while heap and t > heap[0][0]:
+        #         _, ind = heapq.heappop(heap)
+        #         ans[ind] = i - ind
 
-            heapq.heappush(heap, (t, i))
+        #     heapq.heappush(heap, (t, i))
 
-        return ans
+        # return ans
+        stack = []
+        sol = [0] * n
+        for i in range(n):
+            temp = temperatures[i]
+
+            while stack and stack[-1][0] <  temp:
+                [value, index] = stack.pop()
+                sol[index] = i - index
+            
+            stack.append([temperatures[i], i])
+        return sol
