27423b178a6bdf89b36b8e533412c99bd870927b
Ajay Patel <61546968+ajp4891@users.noreply.github.com>
2024-10-17 17:27:28 -0700

    Time: 405 ms (99.53%) | Memory: 27.8 MB (15.14%) - LeetSync

---
+++ 2690-house-robber-iv/house-robber-iv.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minCapability(self, nums: List[int], k: int) -> int:
+        def in_the_range(stole):
+            count = i = 0
+            while i < len(nums):
+                if nums[i] <= stole:
+                    count += 1
+                    i += 1
+                i += 1
+            if count >= k:
+                return True
+            return False
+
+        minl, maxr = float('inf'), float('-inf')
+
+        for n in nums:
+            minl = min(n, minl)
+            maxr = max(n, maxr)
+
+        while minl <= maxr:
+            mid = minl + (maxr - minl) // 2
+            if in_the_range(mid):
+                maxr = mid - 1
+            else:
+                minl = mid + 1
+
+        return minl
