93b13d2576672e16d80cf0e5f4d6e8490417a87b
Ajay Patel <61546968+ajp4891@users.noreply.github.com>
2024-10-29 00:42:24 -0700

    Time: 858 ms (6.06%) | Memory: 42.5 MB (5.30%) - LeetSync

---
+++ 2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.py
@@ -0,0 +1,34 @@
+class Solution:
+    def maxMoves(self, grid: List[List[int]]) -> int:
+        dp = defaultdict(int)
+        m, n = len(grid), len(grid[0])
+        directions = [(-1, 1), (0, 1), (1, 1)]
+
+        def dfs(r, c, prev_val):
+            print(r, c, not 0 <= r < m, not 0 <= c < n)
+            if not 0 <= r < m or not 0 <= c < n:
+                return 0
+
+            print(prev_val, grid[r][c], prev_val >= grid[r][c])
+            if prev_val >= grid[r][c]:
+                return 0
+
+            if (r, c) in dp:
+                return dp[(r,c)]
+
+            maxval = 0
+            for dr, dc in directions:
+                val = dfs(r + dr, c + dc, grid[r][c])
+                maxval = max(maxval, val)
+            dp[(r, c)] = maxval + 1
+            return dp[(r, c)]
+
+
+        maxans = 0
+        row = 0
+        while row < m:
+            for dr, dc in directions:
+                maxans = max(maxans, dfs(row + dr, dc, grid[row][0]))
+            row += 1
+
+        return maxans
