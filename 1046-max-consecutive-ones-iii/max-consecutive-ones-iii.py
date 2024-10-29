be74a3fa4412aee29aa0f72d19aa6bacc0f740a3
Ajay Patel <61546968+ajp4891@users.noreply.github.com>
2024-10-29 04:10:12 -0700

    Time: 17 ms (97.66%) | Memory: 17.3 MB (23.83%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -1,27 +1,36 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        current_zeros = 0
-        maxones = 0
-        ones = 0
-        i = j = 0
-        while j < len(nums):
-            if nums[j] == 0:
-                if k == 0:
-                    ones = -1
-                    i = j
+        # current_zeros = 0
+        # maxones = 0
+        # ones = 0
+        # i = j = 0
+        # while j < len(nums):
+        #     if nums[j] == 0:
+        #         if k == 0:
+        #             ones = -1
+        #             i = j
                 
-                if current_zeros == k:
-                    while i < j:
-                        ones -= 1
-                        if nums[i] == 0:
-                            break
-                        i += 1
-                    current_zeros -= 1
-                    i += 1
-                current_zeros += 1
+        #         if current_zeros == k:
+        #             while i < j:
+        #                 ones -= 1
+        #                 if nums[i] == 0:
+        #                     break
+        #                 i += 1
+        #             current_zeros -= 1
+        #             i += 1
+        #         current_zeros += 1
 
-            ones += 1                
-            maxones = max(ones, maxones)
-            j += 1
+        #     ones += 1                
+        #     maxones = max(ones, maxones)
+        #     j += 1
 
-        return maxones
+        # return maxones
+        l=r=0    
+        for r in range(len(nums)):
+            if nums[r] == 0:
+                k-=1
+            if k<0:
+                if nums[l] == 0:
+                    k+=1
+                l+=1
+        return r-l+1
