742befe09d3364c616dec452470d30c8cca5aa2c
Ajay Patel <61546968+ajp4891@users.noreply.github.com>
2024-11-14 22:55:30 -0800

    Time: 183 ms (14.58%) | Memory: 42.8 MB (5.75%) - LeetSync

---
+++ 2247-number-of-unique-flavors-after-sharing-k-candies/number-of-unique-flavors-after-sharing-k-candies.py
@@ -0,0 +1,27 @@
+class Solution:
+    def shareCandies(self, candies: List[int], k: int) -> int:
+        l, r, n = 0, 0, len(candies)
+
+        counter = Counter(candies)
+        ans = 0
+        while r < k:
+            if counter[candies[r]] == 1:
+                del counter[candies[r]]
+            else:
+                counter[candies[r]] -= 1
+            r += 1
+
+        ans = len(counter)
+
+        while r < n:
+            if counter[candies[r]] == 1:
+                del counter[candies[r]]
+            else:
+                counter[candies[r]] -= 1
+            r += 1
+
+            counter[candies[l]] += 1
+            l += 1
+            ans = max(ans, len(counter))
+
+        return ans
