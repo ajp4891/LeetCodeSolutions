992ec4aff491ec3d8afdb166e791654f9a262c1f
Ajay Patel <61546968+ajp4891@users.noreply.github.com>
2024-11-17 03:49:49 -0800

    Time: 7 ms (92.68%) | Memory: 27.4 MB (62.15%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -6,7 +6,8 @@ class Solution:
         while r < len(nums):
             total += nums[r]
             while total >= target:
-                ans = min(ans, r - l + 1)
+                if r - l + 1 < ans:
+                    ans = r - l + 1
                 total -= nums[l]
                 l += 1
             r += 1
