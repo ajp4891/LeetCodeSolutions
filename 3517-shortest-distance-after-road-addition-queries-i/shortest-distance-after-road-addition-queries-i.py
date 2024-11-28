12645770a05238320f6e4ffb07dc216ef96663f6
Ajay Patel <61546968+ajp4891@users.noreply.github.com>
2024-11-27 16:28:44 -0800

    Time: 47 ms (100.00%) | Memory: 17.4 MB (8.40%) - LeetSync

--- 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.py
+++ 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.py
@@ -1,34 +1,26 @@
 class Solution:
-    # Function to find the minimum distance from node 0 to node n-1
-    def find_min_distance(self, adj_list, n):
-        dp = [0] * n
-        dp[n - 1] = 0  # Base case: distance to destination (n-1) is 0
+    def shortestDistanceAfterQueries(self, n: int, queries: List[List[int]]) -> List[int]:
+        adj = defaultdict(list)
+        for i in range(n-1):
+            adj[i].append(i+1)
+        
+        depth = [i for i in range(n)]
 
-        # Iterate from the second last node down to the first node
-        for current_node in range(n - 2, -1, -1):
-            min_distance = n
-            # Explore neighbors to find the minimum distance
-            for neighbor in adj_list[current_node]:
-                min_distance = min(min_distance, dp[neighbor] + 1)
-            # Store the calculated distance for the current node
-            dp[current_node] = min_distance
-
-        return dp[0]
-
-    def shortestDistanceAfterQueries(self, n, queries):
-        answer = []
-        adj_list = [[] for _ in range(n)]
-
-        # Initialize edges between consecutive nodes
-        for i in range(n - 1):
-            adj_list[i].append(i + 1)
-
-        # Process each query to add new edges
-        for road in queries:
-            u, v = road[0], road[1]
-            adj_list[u].append(v)  # Add the directed edge from u to v
-
-            # Calculate the minimum distance after adding the new edge
-            answer.append(self.find_min_distance(adj_list, n))
-
-        return answer
+        def bfs(node):
+            q = deque([node])
+            while q:
+                n = q.popleft()
+                for nei in adj[n]:
+                    if depth[nei] > depth[n]+1:
+                        depth[nei] = depth[n]+1
+                        q.append(nei)
+        
+        ans = []
+        for s, e in queries:
+            adj[s].append(e)
+            if depth[e] > depth[s]+1:
+                depth[e] = depth[s]+1
+                bfs(e)
+            ans.append(depth[-1])
+        return ans
+            
