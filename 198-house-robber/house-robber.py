5d6787dda917d279e5afb59cf2393987b59ea204
Ajay Patel <61546968+ajp4891@users.noreply.github.com>
2025-01-16 10:21:15 -0800

    Time: 0 ms (100.00%) | Memory: 17.6 MB (40.57%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,19 +1,34 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n = len(nums)
-        max_stolen = 0
-        dp = {}
-        def dfs(current_house, stole_so_far):
-            if current_house >= n:
-                return stole_so_far
+        # n = len(nums)
+        # max_stolen = 0
+        # dp = {}
+        # def dfs(current_house, stole_so_far):
+        #     if current_house >= n:
+        #         return stole_so_far
 
-            if current_house in dp:
-                return dp[current_house]
+        #     if current_house in dp:
+        #         return dp[current_house]
             
-            skip = dfs(current_house + 1, stole_so_far)
-            current = dfs(current_house + 2, stole_so_far) + nums[current_house]
+        #     skip = dfs(current_house + 1, stole_so_far)
+        #     current = dfs(current_house + 2, stole_so_far) + nums[current_house]
 
-            dp[current_house] = max(skip, current)
-            return dp[current_house]
+        #     dp[current_house] = max(skip, current)
+        #     return dp[current_house]
 
-        return dfs(0, 0)
+        # return dfs(0, 0)
+
+        if len(nums) == 0 or nums is None:
+            return 0
+
+        if len(nums) == 1:
+            return nums[0]
+
+        t1 = 0
+        t2 = 0
+        for current in nums:
+            temp = t1
+            t1 = max(current + t2, t1)
+            t2 = temp
+
+        return t1
