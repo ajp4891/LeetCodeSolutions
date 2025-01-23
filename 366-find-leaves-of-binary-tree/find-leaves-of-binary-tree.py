13963c3d2b647911547ca9d265e91551c3b9f4a4
Ajay Patel <61546968+ajp4891@users.noreply.github.com>
2025-01-23 12:46:52 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (25.17%) - LeetSync

--- 366-find-leaves-of-binary-tree/find-leaves-of-binary-tree.py
+++ 366-find-leaves-of-binary-tree/find-leaves-of-binary-tree.py
@@ -6,42 +6,61 @@
 #         self.right = right
 class Solution:
     def findLeaves(self, root: Optional[TreeNode]) -> List[List[int]]:
-        parents = defaultdict(set)
-        out_degree = defaultdict(int)
-        queue = deque([root])
-        while queue:
-            parent = queue.popleft()
-            out_degree[parent] = 0 
-            for node in [parent.left, parent.right]:
-                if node:
-                    queue.append(node)
-                    parents[node].add(parent)
-                    out_degree[parent] += 1
+        ans = []
 
-        queue = deque()
-        for node, degree in out_degree.items():
-            if degree == 0:
-                queue.append(node)
+        def dfs(node):
+            if not node:
+                return -1
 
-        # # print(parents)
-        # for k, v in parents.items():
-        #     print(k.val, [i.val for i in v])
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            lvl = max(left, right) + 1
+
+            if len(ans) <= lvl:
+                ans.append([])
+
+            ans[lvl].append(node.val)
+            return lvl
+
+        dfs(root)
+        return ans
+        # parents = defaultdict(set)
+        # out_degree = defaultdict(int)
+        # queue = deque([root])
+        # while queue:
+        #     parent = queue.popleft()
+        #     out_degree[parent] = 0 
+        #     for node in [parent.left, parent.right]:
+        #         if node:
+        #             queue.append(node)
+        #             parents[node].add(parent)
+        #             out_degree[parent] += 1
+
+        # queue = deque()
+        # for node, degree in out_degree.items():
+        #     if degree == 0:
+        #         queue.append(node)
+
+        # # # print(parents)
+        # # for k, v in parents.items():
+        # #     print(k.val, [i.val for i in v])
         
-        # for k, v in out_degree.items():
-        #     print(k.val, v)
+        # # for k, v in out_degree.items():
+        # #     print(k.val, v)
         
-        res = []
-        while queue:
-            leaves = []
-            n = len(queue)
-            for _ in range(n):
-                node = queue.popleft()
-                if node != root:
-                    for parent in parents[node]:
-                        out_degree[parent] -= 1
-                        if out_degree[parent] == 0:
-                            queue.append(parent)
-                leaves.append(node.val)
-            res.append(leaves)
+        # res = []
+        # while queue:
+        #     leaves = []
+        #     n = len(queue)
+        #     for _ in range(n):
+        #         node = queue.popleft()
+        #         if node != root:
+        #             for parent in parents[node]:
+        #                 out_degree[parent] -= 1
+        #                 if out_degree[parent] == 0:
+        #                     queue.append(parent)
+        #         leaves.append(node.val)
+        #     res.append(leaves)
 
-        return res
+        # return res
