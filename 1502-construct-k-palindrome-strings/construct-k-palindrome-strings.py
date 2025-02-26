6455fdc6ba9b20acafb1496a843fd423b427fc41
Ajay Patel <61546968+ajp4891@users.noreply.github.com>
2025-02-26 14:18:04 -0800

    Time: 62 ms (15.69%) | Memory: 18 MB (78.29%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
@@ -6,14 +6,19 @@ class Solution:
         if len(s) == k:
             return True
 
-        freq = [0] * 26
-        odds = 0
+        # freq = [0] * 26
+        # odds = 0
 
-        for c in s:
-            freq[ord(c) - ord('a')] += 1
+        # for c in s:
+        #     freq[ord(c) - ord('a')] += 1
 
-        for cnts in freq:
-            if cnts % 2 == 1:
-                odds += 1
+        # for cnts in freq:
+        #     if cnts % 2 == 1:
+        #         odds += 1
 
-        return odds <= k
+        # return odds <= k
+        odd_count = 0
+
+        for chr in s:
+            odd_count ^= 1 << (ord(chr) - ord("a"))
+        return bin(odd_count).count("1") <= k
