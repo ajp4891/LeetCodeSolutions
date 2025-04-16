22fcabe700d069af74a073ef0456f135b7318711
Ajay Patel <61546968+ajp4891@users.noreply.github.com>
2025-04-16 13:05:59 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (30.69%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -1,18 +1,34 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        d = defaultdict(set)
+        # d = defaultdict(set)
+
+        # target = len(graph) - 1
+        # res = []
+        # q = deque()
+        # q.append([0])
+
+        # while q:
+        #     temp = q.popleft()
+        #     if temp[-1] == target:
+        #         res.append(temp)
+            
+        #     for nei in graph[temp[-1]]:
+        #         q.append(temp + [nei])
+
+        # return res
 
         target = len(graph) - 1
         res = []
-        q = deque()
-        q.append([0])
 
-        while q:
-            temp = q.popleft()
-            if temp[-1] == target:
-                res.append(temp)
-            
-            for nei in graph[temp[-1]]:
-                q.append(temp + [nei])
+        def dfs(node, path):
+            if node == target:
+                res.append(path[:])
+                return
 
+            for nei in graph[node]:
+                path.append(nei)
+                dfs(nei, path)
+                path.pop()
+
+        dfs(0, [0])
         return res
